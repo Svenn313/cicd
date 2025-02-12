e547648ec8135024a5619744734e5118be98e566
Svenn313 <pierre.sephiha@hotmail.fr>
2025-02-12 10:12:57 +0100

    add calc

---
+++ calc.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def addition(a,b):
+    print(a+b)
+
+def soustraction(a,b):
+    print(a-b)
+
+def multiplication(a,b):
+    print(a*b)
+
+def division(a,b):
+    print(a//b)
